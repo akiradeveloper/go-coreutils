2b804aa4c20ea08a8064280bed7a5267290ecd7b
Akira Hayakawa <ruby.wktk@gmail.com>
2014-07-23 12:09:06 +0900

    impl more ^^
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- tsort/tsort.go
+++ tsort/tsort.go
@@ -39,6 +39,58 @@ var (
 	version = flag.Bool("version", false, version_text)
 )
 
+// TODO optimization: use int
+type V string
+
+type Node struct {
+	in_edges  map[V]bool
+	out_edges []V
+}
+
+func NewNode() *Node {
+	n := Node{}
+	n.in_edges = make(map[V]bool)
+	n.out_edges = make([]V, 0)
+	return &n
+}
+
+type Graph struct {
+	nodes  map[V]*Node
+	result []V
+}
+
+func NewGraph() *Graph {
+	g := Graph{}
+	g.nodes = make(map[V]*Node)
+	g.result = make([]V, 0)
+	return &g
+}
+
+func (g *Graph) initNode(n V) {
+	g.nodes[n] = NewNode()
+}
+
+func (g *Graph) hasNode(n V) bool {
+	_, ok := g.nodes[n]
+	return ok
+}
+
+func (g *Graph) addEdge(from V, to V) {
+	if !g.hasNode(from) {
+		g.initNode(from)
+	}
+	if !g.hasNode(to) {
+		g.initNode(to)
+	}
+	_, ok := g.nodes[to].in_edges[from]
+	if ok {
+		return
+	}
+	out_edges := g.nodes[from].out_edges
+	out_edges = append(out_edges, to)
+	g.nodes[to].in_edges[from] = true
+}
+
 func main() {
 	flag.Parse()
 	if *help {
@@ -71,13 +123,21 @@ func main() {
 		os.Exit(1)
 	}
 
+	g := NewGraph()
 	scanner := bufio.NewScanner(fp)
 	for scanner.Scan() {
 		var nodes = strings.Fields(scanner.Text())
-		if len(nodes) > 2 {
+		N := len(nodes)
+		if N > 2 {
 			fmt.Fprintf(os.Stdout, "%s: input contains an odd number of tokens\n", input)
 			os.Exit(1)
+		} else if N == 1 {
+			// TODO
+			// 1 \n 2 3 \n 4 is allowed but
+			// 1 \n 2 3 is not allowed
+			os.Exit(1)
 		}
+		g.addEdge(V(nodes[0]), V(nodes[1]))
 	}
 
 	os.Exit(0)
